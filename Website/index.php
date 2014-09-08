29c497ba92a539c597a6d824f1ce01eafbb4c9b1
NathanWild <Nathan_Wild@hotmail.co.uk>
2014-09-08 01:18:59 +0100

    Test

--- Website/index.php
+++ Website/index.php
@@ -2,7 +2,32 @@
 <html>
 	<head>
 		<title>Imaginative Design></title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+		<script>
+			$(document).ready(function() {
+				$('#content').slideDown(2000);
+			});
+		</script>
+
 	</head>
+	
 	<body>
+	
+	<div id="content" style="display:none;" >
+	
+		<center>
+		<img id="logo" src="images/logo_200.png">
+		
+		<br>
+		<br>
+		
+		If you can imagine it, we can create it.
+		
+		</center>
+	
+	</div>
+	
 	</body>
 </html>
